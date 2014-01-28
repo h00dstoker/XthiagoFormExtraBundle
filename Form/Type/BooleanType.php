f69788a26fe1423d2b16f6c892ea4bc28b14a4b2
Thiago Rodrigues <xthiago@gmail.com>
2014-01-28 10:24:30 -0200

    adds boolean and spinner types - yet needs css

---
+++ Form/Type/BooleanType.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace Xthiago\FormExtraBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormView\FormInterface;
+use Symfony\Component\Form\FormBuilderInterface;
+
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
+
+
+class BooleanType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(
+            array(
+                'label' => false,
+                'styleType' => '2', // em pt é: 2 e 7.
+            )
+        );
+    }
+
+    public function buildView(\Symfony\Component\Form\FormView $view, \Symfony\Component\Form\FormInterface $form, array $options)
+    {
+        $view->vars['styleType'] = array_key_exists('styleType', $options) ? $options['styleType'] : '2';
+
+        parent::buildView($view, $form, $options);
+    }
+
+    public function getParent()
+    {
+        return 'checkbox';
+    }
+
+    public function getName()
+    {
+        return 'xthiago_boolean';
+    }
+}
