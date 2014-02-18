cab0e762750ca5994795c7be93d8972865a14d2a
Aline Arielo <aline.s.arielo@gmail.com>
2014-02-18 12:12:10 -0300

    inclusao de parametros pra rota no select 2 async

--- Form/Type/SelectAsyncType.php
+++ Form/Type/SelectAsyncType.php
@@ -28,6 +28,7 @@ class SelectAsyncType extends AbstractType
     public function buildView(\Symfony\Component\Form\FormView $view, \Symfony\Component\Form\FormInterface $form, array $options)
     {
         $view->vars['data_source_route'] = array_key_exists('data_source_route', $options) ? $options['data_source_route'] : null;
+        $view->vars['route_parameters'] = array_key_exists('route_parameters', $options) ? $options['route_parameters'] : null;
         $view->vars['multiple'] = array_key_exists('multiple', $options) ? $options['multiple'] : null;
         $view->vars['allowcreate'] = array_key_exists('allowcreate', $options) ? $options['allowcreate'] : null;
         $view->vars['placeholder'] = array_key_exists('placeholder', $options) ? $options['placeholder'] : null;
